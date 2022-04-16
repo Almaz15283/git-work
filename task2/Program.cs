8e52d299b607d8593491995ad7477796374dd518
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-16 18:12:07 +0300

    нахождение максимального числа из трех

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!");
+int a = 44;
+int b =5;
+int c = 78;
+int max = 0;
+if (a>b) 
+   {
+        max = a;
+    if (max>c)
+    max = max;
+    else max = c;
+   }
+else {max = b;
+    if (max>c)
+    max = max;
+    else max = c;
+}
+Console.Write("max number = ");
+Console.Write(max);
