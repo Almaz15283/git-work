de746075c7789531f0b9b5009d5daee75f7f0966
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-16 20:23:01 +0300

    task3 введите число и определите его четность

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!");
+int a = new Random().Next(1, 20);
+
+Console.Write("число А = ");
+Console.Write(a);
+double b = a % 2;
+if ( b == 0)
+    {
+        Console.Write(" -число четное");
+    }
+    else 
+    {
+        Console.Write(" -число не четное");
+    }
