1d8f4657de351b69a34ca56e56b397b9c86dab70
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-16 20:51:54 +0300

    task4 вывести все четные числа от 1 до N

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!");
+int a = 1;
+int i = 0;
+int N = new Random().Next(3, 20);
+
+while (i <= N)
+
+{
+    int b = i%2;
+    
+    if (b == 0) 
+    {
+        Console.Write(i);
+    }
+    
+i++;
+}
