f989bff8d30646b6b10e4f8087d7e2ec4d9a049b
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-16 13:41:20 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, user");
+int a =5;
+int b =7;
+    if (a > b)
+            {
+            Console.Write("max number = ");
+            Console.Write(a);
+            }
+    else 
+            {
+            Console.Write("max number = ");
+            Console.Write(b);
+            }
+            
+
